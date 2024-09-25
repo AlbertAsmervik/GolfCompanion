153d65061301de62d0829b7f3b98683773a0f083
AlbertAsmervik <Albertasmervik@gmail.com>
2024-09-25 20:51:04 -0300

    README

--- GolfApi/obj/Debug/net8.0/GolfApi.AssemblyInfo.cs
+++ GolfApi/obj/Debug/net8.0/GolfApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolfApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17075d8df921cdf1ec9af7fe16eca4913beb18a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3a5649019c272b391169790f91c0e82a714fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolfApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolfApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,12 @@
+# GolfCompanion
+Project to explore .net and angular
+
+# DB
+remember to check connection to dataStudio
+and auto seed docker
+# Front
+` ng serve `
+
+# Api
+` dotnet run --launch-profile https `
+
